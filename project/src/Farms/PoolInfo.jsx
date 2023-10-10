b532f14b1514930a5c907c978cb6d482e8c0a2fb
MrNjordy <145882008+MrNjordy@users.noreply.github.com>
2023-10-10 14:26:29 +1300

    Update PoolInfo.jsx

--- project/src/Farms/PoolInfo.jsx
+++ project/src/Farms/PoolInfo.jsx
@@ -130,7 +130,7 @@ export default function FarmPoolInfo({
             <Box fontFamily='heading' mt={5} padding={3} paddingBottom={1} width={250} bgColor='gray.900' fontWeight='semibold' color='gray.300' border={isAtrofa ? '2px' : 'none'} borderBottom='none' borderColor='yellow.500'>
                 <HStack>
                 {/* <Image src={`https://github.com/MrNjordy/Atrofarm/blob/main/project/src/assets/FarmIcons/${token0}.png`} mb={3}></Image>  
-                <Image src={`https://github.com/MrNjordy/Atrofarm/blob/main/project/src/assets/FarmIcons/${token1}.png`} mb={3}></Image>     */}
+                <Image src={`https://github.com/MrNjordy/Atrofarm/tree/main/project/src/assets/FarmIcons/${token1}.png`} mb={3}></Image>     */}
                         <Flex mb={3} mr={1} ml='auto'>
                         <Text color='gray.300'>{name}</Text>
                         </Flex>   

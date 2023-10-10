81da20faedaf5dd01bc044a29858faf129e75f7b
MrNjordy <145882008+MrNjordy@users.noreply.github.com>
2023-10-11 09:52:22 +1300

    ..

--- project/src/Farms/Farm.jsx
+++ project/src/Farms/Farm.jsx
@@ -92,7 +92,7 @@ return(
     <Box minHeight='100vh'>
             <VStack>
             <Box height={[120, 130, 150,220]} bgGradient='linear(to-bl, yellow.400, yellow.700)' width='100%' padding={[5, null, null, 10]}>
-                <Center>
+                <Center mt='auto' mb='auto'>
                 <Text fontFamily='heading' fontWeight='bold' fontSize={[null, 20, 30, 40]} color='black' ml={[10,20,30,40]} mr={[10,20,30,40]} align='center'>
                 Farming Pools
                 </Text>
